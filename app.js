5b51bf0e9f8ac1b4a846e57357f4c39a64327813
xerxes324 <206946888+xerxes324@users.noreply.github.com>
2025-08-27 13:46:46 +0530

    Initial commit

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
-const express = require('express');
+const userRouter = require("./routes/userRouter");
+const express = require("express");
 const app = express();
-const path = require("node:path");
-
-app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
-app.use(express.static("public"));
+app.use(express.urlencoded({extended: true}));
+
+const PORT = process.env.PORT || 8080;
+
+app.use("/", userRouter);
+
+app.listen(PORT, (error)=>{
+
+    if ( error){
+        throw error;
+    }
+
+    console.log(`Server is running on PORT ${PORT}.`);
+})
