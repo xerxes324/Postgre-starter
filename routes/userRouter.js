d2c6faf18957c497aaec723fd047e2c4f280d8eb
xerxes324 <206946888+xerxes324@users.noreply.github.com>
2025-08-28 00:51:41 +0530

    route to handle requests

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -2,7 +2,9 @@ const {Router} = require("express");
 const userController = require("../controllers/userController");
 const userRouter = Router();
 
-userRouter.get("/", userController.userGet);
-userRouter.get("/new", userController.userFormGet);
-userRouter.post("/new", userController.userFormPost);
+userRouter.get("/", userController.getUsernames);
+userRouter.get("/new", userController.createUsernameGet);
+userRouter.post("/new", userController.createUsernamePost);
+userRouter.get("/delete", userController.getDeleteUser);
+
 module.exports = userRouter;
