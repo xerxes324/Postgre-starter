e0f75e3fabd8ef9573da66b6d126b599e276052b
xerxes324 <206946888+xerxes324@users.noreply.github.com>
2025-08-28 00:51:06 +0530

    express server setup

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const userRouter = require("./routes/userRouter");
 const express = require("express");
 const app = express();
+require("dotenv").config();
+
 app.set("view engine", "ejs");
 app.use(express.urlencoded({extended: true}));
 
